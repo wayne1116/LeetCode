c02e47ae3c37781671a75b9954c2e6122bdf9444
Wayne Chen <wayne861116@gmail.com>
2020-01-18 14:04:15 +0800

    Update Number_of_Burgers_with_No_Waste_of_Ingredients.java

--- Weekly Contest/Weekly Contest(161-170)/Weekly Contest 165/Number_of_Burgers_with_No_Waste_of_Ingredients.java
+++ Weekly Contest/Weekly Contest(161-170)/Weekly Contest 165/Number_of_Burgers_with_No_Waste_of_Ingredients.java
@@ -1,5 +1,9 @@
 class Solution {
     public List<Integer> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+        // 4x+y=a
+        // 2x+y=b
+        // x=(a-2b)/2
+        // y=(-a+4b)/2
         List<Integer> result=new ArrayList<Integer>();
         int det1=tomatoSlices-(cheeseSlices<<1);
         int det2=-tomatoSlices+(cheeseSlices<<2);

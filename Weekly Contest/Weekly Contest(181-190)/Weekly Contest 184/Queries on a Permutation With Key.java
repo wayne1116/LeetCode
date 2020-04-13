8860966b2ab4eab8f4f1cb69c2bbf5348ef279e7
Wayne Chen <wayne861116@gmail.com>
2020-04-13 13:07:03 +0800

    Update Queries on a Permutation With Key.java

--- Weekly Contest/Weekly Contest(181-190)/Weekly Contest 184/Queries on a Permutation With Key.java
+++ Weekly Contest/Weekly Contest(181-190)/Weekly Contest 184/Queries on a Permutation With Key.java
@@ -19,3 +19,47 @@ class Solution {
         return result;
     }
 }
+
+/* Other version: Binary indexed tree solution
+
+class Solution {
+    public int[] processQueries(int[] queries, int m) {
+        int n=queries.length;
+        int[] bits=new int[n+m+1];
+        Arrays.fill(bits, 0);
+        
+        Map<Integer, Integer> map=new HashMap<Integer, Integer>();
+        for(int i=1; i<=m; i++){
+            update(bits, n+i, 1);
+            map.put(i, n+i);
+        }
+        
+        int[] result=new int[n];
+        for(int i=0; i<n; i++) {
+            int index=map.get(queries[i]);
+            result[i]=prefixsum(bits, index)-1;
+            update(bits, index, -1);
+            update(bits, n-i, 1);
+            map.put(queries[i], n-i);
+        }
+        return result;
+    }
+    
+    void update(int[] bits, int index, int val) {
+        while(index<bits.length) {
+            bits[index]+=val;
+            index+=(index&(-index));
+        }
+    }
+    
+    int prefixsum(int[] bits, int index) {
+        int sum=0;
+        while(index>0) {
+            sum+=bits[index];
+            index-=(index&(-index));
+        }
+        return sum;
+    }
+}
+
+*/

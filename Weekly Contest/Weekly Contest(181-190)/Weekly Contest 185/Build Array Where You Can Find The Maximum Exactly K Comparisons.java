7413b89f4942562346e2a50f942ba92d547e2202
Wayne Chen <wayne861116@gmail.com>
2020-04-22 23:28:37 +0800

    Update Build Array Where You Can Find The Maximum Exactly K Comparisons.java

--- Weekly Contest/Weekly Contest(181-190)/Weekly Contest 185/Build Array Where You Can Find The Maximum Exactly K Comparisons.java
+++ Weekly Contest/Weekly Contest(181-190)/Weekly Contest 185/Build Array Where You Can Find The Maximum Exactly K Comparisons.java
@@ -9,7 +9,15 @@ class Solution {
         
         for(int i=1; i<=m; i++)
             dp[1][i][1]=1;
-        
+        /*
+            dp[a][b][c]:
+            dp[a][b][c]+=j*dp[a-1][b][c]
+            an array of length i - 1 with maximum element equal to j at a search cost of k,
+            then just append any element from [1, 2, ... j]
+            
+            dp[a][b][c]+=SUM(dp[a-1][x][c-1]) from x=1 to b-1
+            append the element j to the end of this array
+        */
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
                 for(int l=1; l<=k; l++){

aae60c1beeb360b0d5842b6d958d036565f0340c
Wayne Chen <wayne861116@gmail.com>
2020-07-15 18:14:39 +0800

    Create Best Position for a Service Centre.java

---
+++ Weekly Contest/Weekly Contest(191-200)/Weekly Contest 197/Best Position for a Service Centre.java
@@ -0,0 +1,50 @@
+class Solution {
+    double calcudis(double startx, double starty, int px, int py) {
+        double t1=startx-px;
+        double t2=starty-py;
+        
+        return Math.sqrt(t1*t1+t2*t2);
+    }
+    public double getMinDistSum(int[][] positions) {
+        double startx=0;
+        double starty=0;
+        for(int i=0; i<positions.length; i++) {
+            startx+=positions[i][0];
+            starty+=positions[i][1];
+        }
+        startx/=positions.length;
+        starty/=positions.length;
+        double prev=0;
+        double curr=0;
+        for(int i=0; i<positions.length; i++)
+            curr+=calcudis(startx, starty, positions[i][0], positions[i][1]);
+        
+        while(Math.abs(prev-curr)>0.0000001){
+            double v=0;
+            double x=0.00001;
+            double y=0.00001;
+            for(int i=0; i<positions.length; i++) {
+                double t;
+                if(startx==positions[i][0] && starty==positions[i][1])
+                    t=0;
+                else 
+                    t=1/calcudis(startx, starty, positions[i][0], positions[i][1]);
+                x+=positions[i][0]*t;
+                y+=positions[i][1]*t;
+                v+=t;
+            }
+            x/=v;
+            y/=v;
+            
+            prev=curr;
+            curr=0;
+            for(int i=0; i<positions.length; i++)
+                curr+=calcudis(x, y, positions[i][0], positions[i][1]);
+            startx=x;
+            starty=y;
+        }
+        
+        return curr;
+        
+    }
+}

a89fb6ac5e85edd9314d82a3148467681d730826
Wayne Chen <wayne861116@gmail.com>
2020-08-22 14:39:42 +0800

    Create Minimum Operations to Make Array Equal.java

---
+++ Weekly Contest/Weekly Contest(201-210)/Weekly Contest 202/Minimum Operations to Make Array Equal.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minOperations(int n) {
+        int result=0;
+        for(int i=1; i<=n; i+=2) {
+            result+=n-i;
+        }
+        
+        return result;
+    }
+}

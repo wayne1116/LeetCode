20e4912036e2df7fabbabfd16c275e3c23aec7bb
Wayne Chen <wayne861116@gmail.com>
2020-08-31 13:41:08 +0800

    Create Most Visited Sector in a Circular Track.java

---
+++ Weekly Contest/Weekly Contest(201-210)/Weekly Contest 203/Most Visited Sector in a Circular Track.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> mostVisited(int n, int[] rounds) {
+        int[] count=new int[n];
+        for(int i=1; i<rounds.length; i++) {
+            int start=rounds[i-1]-1;
+            int end=rounds[i]-1;
+            while(start!=end) {
+                ++count[start];
+                start=(start+1)%n;
+            }
+        }
+        ++count[rounds[rounds.length-1]-1];
+        
+        List<Integer> result=new ArrayList<Integer>();
+        int max=count[0];
+        for(int i=1; i<n; i++)
+            max=Math.max(max, count[i]);
+        for(int i=0; i<n; i++) {
+            if(max==count[i])
+                result.add(i+1);
+        }
+        
+        return result;
+    }
+}

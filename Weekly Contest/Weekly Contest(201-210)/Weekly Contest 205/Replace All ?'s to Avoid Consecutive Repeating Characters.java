b58a6be773a5f1a430cb57b7796625de8c743aaf
Wayne Chen <wayne861116@gmail.com>
2020-09-07 12:29:41 +0800

    Create Replace All ?'s to Avoid Consecutive Repeating Characters.java

---
+++ Weekly Contest/Weekly Contest(201-210)/Weekly Contest 205/Replace All ?'s to Avoid Consecutive Repeating Characters.java
@@ -0,0 +1,39 @@
+# 
+class Solution {
+    public String modifyString(String s) {
+        StringBuilder st=new StringBuilder();
+        int index=0;
+        for(int i=0; i<s.length(); i++) {
+            if(s.charAt(i)=='?') {
+                if(st.length()>0 && i+1<s.length()) {
+                    int c1=st.charAt(st.length()-1)-'a';
+                    int c2=s.charAt(i+1)-'a';
+                    while(index==c1 || index==c2)
+                        index=(index+1)%26;
+                    
+                    char c=(char)(index+'a');
+                    st.append(c+"");
+                    index=(index+1)%26;
+                }
+                else if(st.length()>0) {
+                    char c=(char)((st.charAt(st.length()-1)-'a'+1)%26+'a');
+                    st.append(c+"");
+                }
+                else if(i+1<s.length() && s.charAt(i+1)!='?') {
+                    char c=(char)((s.charAt(i+1)-'a'+1)%26+'a');
+                    System.out.println((int)c);
+                    st.append(c+"");
+                }
+                else{
+                    char c=(char)(index+'a');
+                    st.append(c+"");
+                    index=(index+1)%26;
+                }
+            }
+            else
+                st.append(s.charAt(i)+"");
+        }
+        
+        return st.toString();
+    }
+}
